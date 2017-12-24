34211637885db44edbf89287b0e24c613f022adf
hehao <hehao@linkface.cn>
2017-12-24 13:07:22 +0800

    Add new solution

--- offer-16/offer-16/main.cpp
+++ offer-16/offer-16/main.cpp
@@ -53,7 +53,21 @@ public:
         
         return result;
     }
+
+    ListNode* Merge2(ListNode* pHead1, ListNode* pHead2) {
+        if(pHead1 == NULL) return pHead2;
+        if(pHead2 == NULL) return pHead1;
+
+        if(pHead1->val < pHead2->val) {
+            pHead1->next = Merge2(pHead1->next, pHead2);
+            return pHead1;
+        } else {
+            pHead2->next = Merge2(pHead1, pHead2->next);
+            return pHead2;
+        }
+    }
 };
+
 
 int main(int argc, const char * argv[]) {
     struct ListNode n1 {1};
