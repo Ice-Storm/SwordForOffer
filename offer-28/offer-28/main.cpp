272b41f079d0155c6a24877030de895b443ca4d3
hehao <hehao@linkface.cn>
2018-01-09 19:58:58 +0800

    Add new solution

--- offer-28/offer-28/main.cpp
+++ offer-28/offer-28/main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 /**
  * 需要注意的是 len 变量的类型要是float，负责在后面 floor((len / 2)) 的时候会截断小数部分，导致floor没有向下取整
- * /
+ */
 
 class Solution {
 public:
@@ -38,6 +38,38 @@ public:
             }
         }
         return 0;
+    }
+    
+    /**
+     * 解法二:
+     *    当一个数字占数组超过一半必然有两个相同数字相邻的情况
+     *    第一个循环找出相领的，但并不一定是超过半数的，如果必然数组有超过半数的就可以找出
+     *    第二个循环最终确认是否是超过半数的
+     */
+    
+    int MoreThanHalfNum_Solution2(vector<int> numbers) {
+        if(numbers.size() <= 0) return 0;
+        
+        int result = numbers[0];
+        int times = 1;
+        
+        for(int i = 0; i < numbers.size(); i++) {
+            if(numbers[i] == result) {
+                times++;
+            } else if(times <= 0) {
+                result = numbers[i];
+                times = 1;
+            } else {
+                times--;
+            }
+        }
+        
+        times = 0;
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers[i] == result) times++;
+        }
+        
+        return (numbers.size() / 2) >= times ? 0 : result;
     }
 };
 
